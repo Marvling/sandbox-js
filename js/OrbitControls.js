b1f2ecc8012dfc786b602b7757eb6c47b3302d8e
Marvling <33942399+Marvling@users.noreply.github.com>
2020-09-11 14:17:10 -0700

    Progress & organizing

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -1,17 +1,17 @@
 
-// import {
-//   EventDispatcher,
-//   MOUSE,
-//   Quaternion,
-//   Spherical,
-//   TOUCH,
-//   Vector2,
-//   Vector3
-// } from "./three.module.js";
+import {
+  EventDispatcher,
+  MOUSE,
+  Quaternion,
+  Spherical,
+  TOUCH,
+  Vector2,
+  Vector3
+} from "./three.module.js";
 
 // This set of controls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
-//
+
 //    Orbit - left mouse / touch: one-finger move
 //    Zoom - middle mouse, or mousewheel / touch: two-finger spread or squish
 //    Pan - right mouse, or left mouse + ctrl/meta/shiftKey, or arrow keys / touch: two-finger move
---
+++ three_v01.js
@@ -0,0 +1,3 @@
+import * as THREE from './js/three.module.js';
+import { OrbitControls } from './js/OrbitControls.js';
+import { GLTFLoader } from './js/GLTFLoader.js';
