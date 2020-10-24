381e2dc8a09c06ede328e5cbc07c2ad9bed254ab
Marvling <33942399+Marvling@users.noreply.github.com>
2020-10-23 21:03:49 -0700

    working on single-page layout

--- single-page/main.js
+++ single-page/main.js
@@ -1,31 +1,26 @@
-function getContent(fragmentId, callback) {
+function loadContent(){
+    let contentDiv = document.getElementById("app");
+    contentDiv.innerHTML = location.hash;
 
-    var pages = {
-        home: "This is the Home page. Welcome to my site.",
-        about: "This page will describe what my site is about",
-        contact: "Contact me on this page if you have any questions"
-    };
-
-    callback(pages[fragmentId]);
+    let homediv = document.querySelector("#home");
+    homediv.classList.add('.hidden');
 }
 
+  
+// if(!location.hash) {
+//     location.hash = "#home";
+// }
+  
+//   loadContent();
+  
+  window.addEventListener("hashchange", loadContent );
+
+  //on hash change remove .hidden from every div
+  //on hash change add .hidden to every div except location.hash
+
+  function master() {
+      const hiddenDivs = document.querySelectorAll('.hidden')
+      hiddenDivs.forEach(hidden => hidden.classList.remove('hidden'));
 
 
-function loadContent() {
-
-    var contentDiv = document.getElementById("app"),
-        fragmentId = location.hash.substr(1);
-
-    getContent(fragmentId, function (content) {
-        contentDiv.innerHTML = content;
-    });
-
-}
-
-if (!location.hash) {
-    location.hash = "#home";
-}
-
-loadContent();
-
-window.addEventListener("hashchange", loadContent)
+  }
