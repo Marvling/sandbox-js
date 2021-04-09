2d21bde4c078d1c2d70197c48293b10a1a6e7960
Sinan Ozer <sinan.ozer7@gmail.com>
2021-04-09 16:24:56 -0700

    api test

--- api-test/index.js
+++ api-test/index.js
@@ -1,7 +1,26 @@
-// const fetch = require('node-fetch');
-
 const TMDBUrl = 'https://api.themoviedb.org/3/movie/550?api_key=00decbdccac0d50538a8bdbf8085ce4a'
 
-console.log(fetch(TMDBUrl))
-  // .then(response => response.json())
-  // .then(data => console.log(data));
+
+function setup(){
+
+	noCanvas();
+
+	getTtitle(TMDBUrl).then(results =>{
+		
+		let year = results.date
+		year = year.slice(0,4)
+
+		createP(year)
+		createP(results.title)
+	})
+}
+
+async function getTtitle (url) {
+	let response = await fetch(url)
+	let json = await response.json()
+
+	return {
+		title: json.title,
+		date: json.release_date
+	}
+}
