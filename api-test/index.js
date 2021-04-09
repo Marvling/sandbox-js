635dbb3ec3fe8f16033347d7236cd5065aa24b2a
Marvling <sinan.ozer7@protonmail.com>
2021-04-08 20:11:22 -0700

    testing api

---
+++ api-test/index.js
@@ -0,0 +1,7 @@
+// const fetch = require('node-fetch');
+
+const TMDBUrl = 'https://api.themoviedb.org/3/movie/550?api_key=00decbdccac0d50538a8bdbf8085ce4a'
+
+console.log(fetch(TMDBUrl))
+  // .then(response => response.json())
+  // .then(data => console.log(data));
