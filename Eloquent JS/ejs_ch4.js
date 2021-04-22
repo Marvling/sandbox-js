eb5c5b84bdf5d625b038b6731bc272dabfe6f077
Marvling <sinan.ozer7@gmail.com>
2021-04-22 09:29:19 -0700

    ejs_ch5 finished

--- Eloquent JS/ejs_ch4.js
+++ Eloquent JS/ejs_ch4.js
@@ -120,4 +120,4 @@ function deepEqual(a, b) {
     }
   
     return true;
-  }
+}
--- Eloquent JS/ejs_ch5.js
+++ Eloquent JS/ejs_ch5.js
@@ -1,10 +1,24 @@
 // Higher Order Functions
 
-const arrays = [[0, 1], [2, 3], [4, 5]]
-const flattened = arrays.reduce(
-    (accumulator, currentValue) => accumulator.concat(currentValue)
-)
+function deepEqual(a, b) {
+    if (a === b) return true;
+    
+    if (a == null || typeof a != "object" ||
+        b == null || typeof b != "object") return false;
+  
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+  
+    if (keysA.length != keysB.length) return false;
+  
+    for (let key of keysA) {
+      if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+}
+  
+    return true;
+}
 
+const arrays = [[0, 1], [2, 3], [4, 5,[6,7]]]
+const flatener = (accumulator, currentValue) => accumulator.concat(currentValue)
 
 function myLooper (value, test, update, body){
     
@@ -34,27 +48,48 @@ function myEvery1 (arr, test) {
 
 function myEvery2 (arr, test) {
     const newArr = arr.filter(test)
-    
-    newArr == arr ? true : false
-}
-
-function myEvery3 (arr) {
-    let newArr = arr.filter(n => n < 10);
-
-    console.log(arr);
     console.log(newArr);
-
-    if (newArr == arr) {
-        return true
+    
+    return deepEqual(arr, newArr)
+}
+  
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+            return code >= from && code < to;
+        })) {return script;}
     }
-    else{
-        return false
-    }
+    return null;
 }
 
-console.log(myEvery2([1,2,3,4,5,6], n => n < 10));
+console.log(characterScript(121));
 
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
 
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
 
+        if (known == -1) {
+            counts.push({name, count: 1});
+        } else {
+            counts[known].count++;
+        }
+}
+return counts;
+}
 
-// console.log(myEvery2([1, 3, 1, 5, 9], n => n < 10))
+function textScripts(text) {
+let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+}).filter(({name}) => name != "none");
+
+let total = scripts.reduce((n, {count}) => n + count, 0);
+if (total == 0) return "No scripts found";
+
+return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+}).join(", ");
+}
